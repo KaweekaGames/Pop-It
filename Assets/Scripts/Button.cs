63064304ffe2c60f7b36ddd9ad282a68351bb19e
David Bibb <kaweekagames@gmail.com>
2018-07-11 21:18:53 -0700

    TouchScript

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Color defaultColor;
+    public Color selectedColor;
+    public Vector3 scale;
+    public EaseType scaleEase;
+
+    private Material mat;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        originalScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnTouchDown()
+    {
+        mat.color = selectedColor;
+        gameObject.ScaleTo(scale, .3f, 0f, scaleEase);
+    }
+
+    void OnTouchUp()
+    {
+        mat.color = defaultColor;
+        gameObject.ScaleTo(originalScale, .3f, 0f, scaleEase);
+    }
+
+    void OnTouchStay()
+    {
+        mat.color = selectedColor;
+        gameObject.ScaleTo(scale, .3f, 0f, scaleEase);
+    }
+
+    void OnTouchCancel()
+    {
+        mat.color = defaultColor;
+        gameObject.ScaleTo(originalScale, .3f, 0f, scaleEase);
+    }
+
+}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,29 +5,22 @@ using System.IO;
 
 public class TestScript : MonoBehaviour
 {
-    public string fileName;
-
-    SpriteRenderer spriteRenderer;
-   
-    
-    // Use this for initialization
-    void Start()
-    {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-
-        string[] lines = File.ReadAllLines(fileName);
-
-        string color = lines[0];
-
-        if (color == "red")
-        {
-            spriteRenderer.color = Color.red; 
-        }
-    }
+    public float time = 1f;
+    public float delay = .5f;
+    public Vector3 newPosition;
+    public Vector3 scale;
+    public EaseType moveEase;
+    public EaseType scaleEase;
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    private void OnMouseDown()
+    {
+        gameObject.MoveTo(newPosition, time, delay, moveEase);
+        gameObject.ScaleTo(scale, time, delay, scaleEase);
     }
 }
